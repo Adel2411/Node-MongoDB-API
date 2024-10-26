84bd73751ae54fe9ed0170388e54fa25eacc39a2
Adel2411 <hadjarabadel.2411@gmail.com>
2024-10-26 18:21:09 +0100

    Creating the common config file to avoid code duplication

---
+++ backend/config/common.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+    dbUri: process.env.DB_URI || "",
+    saltWorkFactor: parseInt(process.env.SALT_WORK_FACTOR || "10") || 10,
+    accessTokenTtl: process.env.ACCESS_TOKEN_TTL || "",
+    refreshTokenTtl: process.env.REFRESH_TOKEN_TTL || "",
+    publicKey: process.env.PUBLIC_KEY || "",
+    privateKey: process.env.PRIVATE_KEY || "",
+};
--- backend/config/test.ts
+++ backend/config/test.ts
@@ -1,13 +1,6 @@
-import dotenv from "dotenv";
-
-dotenv.config();
+import commonConfig from "./common";
 
 export default {
-  port: process.env.PORT || 1337,
-  dbUri: process.env.DB_URI || "",
-  saltWorkFactor: parseInt(process.env.SALT_WORK_FACTOR || "10") || 10,
-  accessTokenTtl: process.env.ACCESS_TOKEN_TTL || "",
-  refreshTokenTtl: process.env.REFRESH_TOKEN_TTL || "",
-  publicKey: process.env.PUBLIC_KEY || "",
-  privateKey: process.env.PRIVATE_KEY || "",
+    ...commonConfig,
+    port: process.env.PORT || 1337,
 };
